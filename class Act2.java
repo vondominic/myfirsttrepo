f2b3050ab91ed6f94afe985372df7278c088a3e9
vondominic <162523707+vondominic@users.noreply.github.com>
2024-03-07 09:58:44 +0800

    Add files via upload

---
+++ class Act2.java
@@ -0,0 +1,25 @@
+public class Act2{
+	
+ public static void main (String[] args){
+	 
+ int num1 = 1;
+ byte num2 = 4;
+ short num3 = 7;
+ long num4 = 6;
+ float num5 = 10;
+ double num6 = 34;
+ String name = "Angela";
+ boolean isTrue = true;
+ char alpha = 'A';
+  
+  System.out.println("The value of the integer is " +num1);
+  System.out.println("The value of the byte is " +num2);
+  System.out.println("The value of the short is " +num3);
+  System.out.println("The value of the long is " +num4);
+  System.out.println("The value of the float is " +num5);
+  System.out.println("The value of the double is " +num6);
+  System.out.println("The value of the String is " +name);
+  System.out.println("The value of the boolean is " +isTrue);
+  System.out.println("The value of the char is " +alpha);
+  }
+}
